c574c08f7c45496af0c6ef62f8eaf407b923e8cc
Andres Garcia Vega <agarciav@Andress-MacBook-Pro.local>
2019-05-06 17:51:34 +0200

    First commit

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,33 +7,40 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, changeCityDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "44e0b6ac7fc96976a53cf352b778f083"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
+    var celOrFar : Bool = true
 
     
     //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
+    @IBOutlet weak var temperatureSwitch: UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         //TODO:Set up the location manager here.
-    
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters // The accuracy we need for our GPS location
+        locationManager.requestWhenInUseAuthorization() // Requests the user for permission to use the GPS location (we also change the info.plist)
+        locationManager.startUpdatingLocation() // Sends a message with the location to the method didUpdateLocations or didFailWithError
         
     }
     
@@ -43,7 +50,21 @@ class WeatherViewController: UIViewController {
     /***************************************************************/
     
     //Write the getWeatherData method here:
-    
+    func getWeatherData(url : String, parameters : [String : String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON{
+            response in
+            if (response.result.isSuccess) {
+                print("Success! Got the weather data")
+                let weatherJSON : JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
+            } else {
+                print("Error \(String(describing: response.result.error))")
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+        
+    }
 
     
     
@@ -55,7 +76,24 @@ class WeatherViewController: UIViewController {
    
     
     //Write the updateWeatherData method here:
-    
+    func updateWeatherData (json : JSON) {
+        
+        if let tempResults = json["main"]["temp"].double {
+            if (temperatureSwitch.isOn) {
+                weatherDataModel.temperature = Int(tempResults * 9 / 5 - 459.67)
+                celOrFar = true
+            } else {
+                weatherDataModel.temperature = Int(tempResults - 273.15)
+                celOrFar = false
+            }
+            weatherDataModel.city = json["name"].stringValue
+            weatherDataModel.condition = json["weather"][0]["id"].intValue
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            updateUIWithWeatherData()
+        } else {
+            cityLabel.text = "Weather Unavailable"
+        }
+    }
 
     
     
@@ -65,7 +103,13 @@ class WeatherViewController: UIViewController {
     
     
     //Write the updateUIWithWeatherData method here:
-    
+    func updateUIWithWeatherData() {
+        
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = String(weatherDataModel.temperature) + "°"
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+        
+    }
     
     
     
@@ -76,11 +120,26 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count-1]
+        if (location.horizontalAccuracy > 0) {
+            locationManager.stopUpdatingLocation()
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
@@ -90,14 +149,35 @@ class WeatherViewController: UIViewController {
     
     
     //Write the userEnteredANewCityName Delegate method here:
-    
+    func userEnteredANewCityName(city: String) {
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
 
     
     //Write the PrepareForSegue Method here
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "changeCityName") {
+            
+            let destinationViewController = segue.destination as! ChangeCityViewController
+            destinationViewController.delegate = self
+            
+        }
+    }
     
     
-    
+    @IBAction func uiSwitchChanged(_ sender: UISwitch) {
+        if (temperatureSwitch.isOn && celOrFar == false) {
+            temperatureLabel.text = String(Int(weatherDataModel.temperature * 9 / 5 + 32)) + "°"
+        } else if (temperatureSwitch.isOn && celOrFar == true){
+            temperatureLabel.text = String(weatherDataModel.temperature) + "°"
+        } else if (!temperatureSwitch.isOn && celOrFar == false) {
+            temperatureLabel.text = String(weatherDataModel.temperature) + "°"
+        } else {
+            temperatureLabel.text = String(Int(weatherDataModel.temperature - 32 * 5 / 9)) + "°"
+        }
+    }
     
 }
 
